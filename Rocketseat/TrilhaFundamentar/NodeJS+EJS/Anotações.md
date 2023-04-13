79bd146d77d73a940308bef57858e478176e0868
Monique <moniquecaggiano@gmail.com>
2023-04-13 20:11:49 -0300

    Adding a project in Node + EJS

--- Rocketseat/TrilhaFundamentar/NodeJS+EJS/Anotações.md
+++ Rocketseat/TrilhaFundamentar/NodeJS+EJS/Anotações.md
@@ -6,3 +6,11 @@
 -   Amorzinho
 -   Install ejs
 -   Sintaxe simples
+
+## Criando o servidor
+
+-   A variável app está CARREGANDO as funcionalidades do express
+-   Vamos usar o ejs para renderizar html pela nossa ferramenta (view engine)
+-   Criando uma rota, '/' é a referência do nosso site, como se fosse home
+-   res.render -> a resposta para essa função é renderizar nosso index
+-   O listen é uma função que fica "ouvindo" uma porta, tal porta 8080
--- Rocketseat/TrilhaFundamentar/NodeJS+EJS/server.js
+++ Rocketseat/TrilhaFundamentar/NodeJS+EJS/server.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const app = express();
 
-// A variável app está CARREGANDO as funcionalidades do express
-// Vamos usar o ejs para renderizar html pela nossa ferramenta (view engine)
 app.set('view engine', 'ejs');
 
-// Criando uma rota, '/' é a referencia do nosso site, tipo o home
-// res.render -> a resposta para essa função é renderizar nosso index
 app.get('/', function (req, res) {
     res.render('index');
 });
@@ -14,3 +10,6 @@ app.get('/', function (req, res) {
 app.get('/sobre', function (req, res) {
     res.render('about');
 });
+
+app.listen(8080);
+console.log('Rodando');
