e117b02b921b1264273e4f80a002b3e6edd8d714
Monique <moniquecaggiano@gmail.com>
2023-04-17 14:52:58 -0300

    Add new content to the index page and create new partials for the head, header, and footer

--- Rocketseat/TrilhaFundamentar/NodeJS+EJS/server.js
+++ Rocketseat/TrilhaFundamentar/NodeJS+EJS/server.js
@@ -4,11 +4,31 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.render('index');
+    const items = [
+        {
+            title: "H",
+            message: "Hoseok ta indo pro exercito"
+        },
+        {
+            title: "O",
+            message: "O mais lindo dos sóis"
+        },
+        {
+            title: "B",
+            message: "Bonito e gentil"
+        },
+        {
+            title: "I",
+            message: "Indo embora, mas eu vou esperar 💜"
+        }
+    ];
+    res.render('pages/index.ejs', {
+        qualitys: items,
+    });
 });
 
 app.get('/sobre', function (req, res) {
-    res.render('about');
+    res.render('pages/about');
 });
 
 app.listen(8080);
