35ff2e4ccadbf0a83b6ed4d3f48405023c7e0eeb
Monique <78742336+Eufrazine@users.noreply.github.com>
2024-04-29 19:51:19 -0300

    Delete NovoTec directory

--- NovoTec/Exercícios/Exercício10/Média.js
+++
@@ -1,9 +0,0 @@
-function calcularMedia() {
-    var $nota1 = parseInt(document.getElementById("n1").value);
-    var $nota2 = parseInt(document.getElementById("n2").value);
-    var $nota3 = parseInt(document.getElementById("n3").value);
-
-    var $media = ($nota1 + $nota2 + $nota3) / 3;
-
-    document.getElementById("iResultado").innerHTML = $media;
-}
