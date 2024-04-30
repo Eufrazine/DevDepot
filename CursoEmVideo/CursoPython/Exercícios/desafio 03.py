23bced409195a4f325014e981bec78a306cccaec
Eufrazine <moniquecaggiano@gmail.com>
2024-04-30 16:41:12 -0300

    Adding projects
    Including new projects in DevDepot

---
+++ CursoEmVideo/CursoPython/Exercícios/desafio 03.py
@@ -0,0 +1,4 @@
+pn = input ('Primeiro número ')
+sn = input ('Segundo número ')
+s = pn+sn
+print ('A soma é ', s)
---
+++ CursoEmVideo/CursoPython/Exercícios/teste 01.py
@@ -0,0 +1,4 @@
+nome = input ('qual teu nome? ')
+idade = input ('tua idade? ')
+peso = input ('qual teu peso? ')
+print (nome, idade, peso)
